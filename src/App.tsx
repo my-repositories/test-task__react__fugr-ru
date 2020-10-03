3e0d2ed052754757515ccb03367d735819196f17
Alexander Loktionov <loktionov129@gmail.com>
2020-10-03 16:30:28 +0300

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
-import logo from './logo.svg';
-import './App.scss';
-
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        <Button variant="contained" color="primary">
-          Learn React
-        </Button>
-      </a>
-    </header>
-  </div>
-);
+const App: React.FC = () => <div className="App">Hello World</div>;
 
 export default App;
