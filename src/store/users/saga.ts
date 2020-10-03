1d89121a85627f158866ceb8a31199874aa7d1bc
Alexander Loktionov <loktionov129@gmail.com>
2020-10-03 22:07:40 +0300

    use real data for production

--- src/store/users/saga.ts
+++ src/store/users/saga.ts
@@ -4,7 +4,17 @@ import { usersBigSize, usersSmallSize } from './user';
 import { failUsersLoading, setUsers } from './actions';
 
 function loadUsers(isSmallSize: boolean) {
-  return Promise.resolve(isSmallSize ? usersSmallSize : usersBigSize);
+  if (process.env.NODE_ENV === 'development') {
+    return Promise.resolve(isSmallSize ? usersSmallSize : usersBigSize);
+  }
+
+  const smallUrl =
+    'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+  const bigUrl =
+    'http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+
+  const url = isSmallSize ? smallUrl : bigUrl;
+  return fetch(url).then((response) => response.json());
 }
 
 function* loadUsersSagaWorker(action: { type: string; payload: boolean }) {
