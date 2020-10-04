f655d6c747c59cd1c166ba3b0837291f2fc6b8ef
loktionov129 <loktionov129@users.noreply.github.com>
2020-10-04 17:12:05 +0000

    deploy: 34dde1614451c84db65be9cac377dbbcf9f37902

---
+++ precache-manifest.1a7efb5dfece466152e6c25a136fb6dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3ff327db97e02419c3d768ee563902",
+    "url": "https://my-repositories.github.io/test-task__react__fugr-ru/index.html"
+  },
+  {
+    "revision": "56fdc262cf3a969ec05d",
+    "url": "https://my-repositories.github.io/test-task__react__fugr-ru/static/js/2.234cc0d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://my-repositories.github.io/test-task__react__fugr-ru/static/js/2.234cc0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "777daff595c9566cfd94",
+    "url": "https://my-repositories.github.io/test-task__react__fugr-ru/static/js/main.f3082477.chunk.js"
+  },
+  {
+    "revision": "1bda26e23f3788498bfa",
+    "url": "https://my-repositories.github.io/test-task__react__fugr-ru/static/js/runtime-main.d1ea647f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fffec95a5a054b0d974d3e8042514602.js"
+  "https://my-repositories.github.io/test-task__react__fugr-ru/precache-manifest.1a7efb5dfece466152e6c25a136fb6dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://my-repositories.github.io/test-task__react__fugr-ru/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
